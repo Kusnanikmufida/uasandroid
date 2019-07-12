3ceeb6342230e2d4d34f71a430cec7631fef18b0
Kusnanikmufida <Sunitarani981001@gmail.com>
2019-07-13 02:15:13 +0700

    first commit

---
+++ app/src/main/java/com/fida/uasandroiddong/activity_tampil.java
@@ -0,0 +1,15 @@
+package com.fida.uasandroiddong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fida.uas.R;
+
+public class activity_tampil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil);
+    }
+}
